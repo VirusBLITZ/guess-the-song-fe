3a4990c5ede3534e80865b61bee47f6852d40b94
VirusBLITZ <58221423+VirusBLITZ@users.noreply.github.com>
2023-12-11 07:12:01 +0100

    install auto-animate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
-    ['@pinia/nuxt', { autoImports: 'defineStore' }]
+    ['@pinia/nuxt', { autoImports: 'defineStore' }],
+    '@formkit/auto-animate/nuxt'
   ],
 })
